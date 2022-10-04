6e6ed1a0838455572bc9b52f951ec60e652e7f0a
PratikshaBankar9689 <pratikshabankar5313@gmail.com>
2022-10-04 22:09:33 +0530

    Completed setup

---
+++ src/pages/Movie.page.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import MovieLayoutHoc from '../layout/Movie.Layout';
+
+const MoviePage=() => {
+  return (
+    <div>MoviePage</div>
+  );
+}
+
+export default MovieLayoutHoc(MoviePage);
---
+++ src/pages/Play.page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import DefaultLayoutHoc from '../layout/Default.Layout';
+const PlayPage = () => {
+  return (
+    <div>PlayPage</div>
+  )
+}
+
+export default DefaultLayoutHoc(PlayPage);
